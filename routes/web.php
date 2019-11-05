f8f9b6c8043ab88bc0db324a012703e2f1e39508
3A632018 <f223145@gmail.com>
2019-11-06 01:38:19 +0800

    3-4練習條件式

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,10 @@ Route::get('/', function () {
     //$post->save();
     //$posts=\App\Post::all();
     //dd($posts);
-    $post=\App\Post::find(1);
-    dd($post);
+    //$post=\App\Post::find(1);
+    //dd($post);
+    $post=\App\Post::where('id', '<', 10)->orderBy('id','DESC')->get();
+     dd($post);
+
 });
 
