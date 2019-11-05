cf50005ecabed0d1205e6a5f7b5000089c3660fb
3A632018 <f223145@gmail.com>
2019-11-06 01:36:51 +0800

    3-1 倒入測試十筆資料&3-2使用 all方法

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,12 @@ Route::get('/', function () {
       //  'title'=>'testtitle',
         //'content'=>'testcontent',
     //]);
-    $post = new \App\Post();
-    $post->title = 'test title2';
-    $post->content = 'test content2';
-    $post->save();
+    //$post = new \App\Post();
+    //$post->title = 'test title2';
+    //$post->content = 'test content2';
+    //$post->save();
+    //$posts=\App\Post::all();
+    //dd($posts);
+
 });
 
