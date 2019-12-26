6a932729fe03b699540fde3193c2f2fa6747f263
3A632018 <f223145@gmail.com>
2019-12-26 19:17:13 +0800

    7-4透過關聯將資料印出來

--- routes/web.php
+++ routes/web.php
@@ -47,9 +47,17 @@ Route::get('/', function () {
     //dd($allPosts);
     //$featuredPosts = \App\Post::where('is_feature', 1)->get();
     //dd($featuredPosts);
-    $post = \App\Post::find(4);
-    dd($post);
-    $lastPost = \App\Post::orderBy('id', 'DESC')->first();
-    dd($lastPost);
+    //$post = \App\Post::find(4);
+   // dd($post);
+    //$lastPost = \App\Post::orderBy('id', 'DESC')->first();
+   // dd($lastPost);
+
+    $post = \App\Post::find(1);
+    echo $post->id.'   '.$post->title.'<br><hr>';
+    foreach($post->comments as $comment) {
+        echo '*'.$comment->content.'<br>';
+    }
+
+
 });
 
